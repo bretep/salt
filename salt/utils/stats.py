e19e3f4551ef7da06dde4ca43c1eaf189354c675
Bret Palsson <bretep@gmail.com>
2012-08-19 15:28:47 -0600

    Add basic exception hook for testing

--- salt/utils/stats.py
+++ salt/utils/stats.py
@@ -4,7 +4,26 @@ Setup Statistics
 import sys
 
 
+def _error_reporting():
+    import traceback
+
+    def hook_exceptions(type_, value_, traceback_):
+        lines = traceback.format_exception(type_, value_, traceback_)
+        print '''
+####################################
+#### Unhandled Exception Caught ####
+####################################
+
+{0}
+####################################
+'''.format(''.join(lines))
+
+    sys.excepthook = hook_exceptions
+
+
 class stats():
 
     def __init__(self):
         pass
+
+_error_reporting()

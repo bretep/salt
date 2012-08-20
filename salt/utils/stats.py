667192c72e27338e088196fd9d59949c264a7c9d
Bret Palsson <bretep@gmail.com>
2012-08-19 20:37:26 -0600

    Working error reproting

--- salt/utils/stats.py
+++ salt/utils/stats.py
@@ -2,21 +2,33 @@
 Setup Statistics
 '''
 import sys
+import httplib2
+import json
+
+
+class _web_connection:
+    def __init__(self, connect_args):
+        self.connect_args = connect_args
+        self.conn = httplib2.Http(timeout=1,
+                                  disable_ssl_certificate_validation=True)
+
+    def send(self, data):
+        try:
+            jdata = json.dumps(data)
+            self.conn.request(body=jdata, **self.connect_args)
+        except:
+            pass
 
 
 def _error_reporting():
     import traceback
 
     def hook_exceptions(type_, value_, traceback_):
-        lines = traceback.format_exception(type_, value_, traceback_)
-        print '''
-####################################
-#### Unhandled Exception Caught ####
-####################################
-
-{0}
-####################################
-'''.format(''.join(lines))
+        etype = traceback.format_exception_only(type_, value_)
+        etb = traceback.format_tb(traceback_)
+        e_server.send('''{{'type':'{0}', 'trace':'{1}'}}'''.format(etype, etb))
+        eout = ''.join(traceback.format_exception(type_, value_, traceback_))
+        print eout
 
     sys.excepthook = hook_exceptions
 
@@ -26,4 +38,9 @@ class stats():
     def __init__(self):
         pass
 
+e_server = _web_connection({
+    'method': 'POST',
+    'uri': 'http://localhost/error',
+    'headers': {'Content-Type': 'application/json'}})
 _error_reporting()
+raise Exception('TESTING 123')
